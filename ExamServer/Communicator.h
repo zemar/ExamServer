408c989524514a6691e52543bd311aed4bffa839
Michael <michael.r808@gmail.com>
2014-03-12 12:27:18 -0500

    commit

---
+++ ExamServer/Communicator.h
@@ -0,0 +1,22 @@
+//
+//  Communicator.h
+//  USApp
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Communicator : NSObject <NSStreamDelegate> {
+@public
+	
+	NSString *host;
+	int port;
+}
+
+- (void)setup;
+- (void)open;
+- (void)close;
+- (void)stream:(NSStream *)stream handleEvent:(NSStreamEvent)event;
+- (void)readIn:(NSString *)s;
+- (void)writeOut:(NSString *)s;
+
+@end
